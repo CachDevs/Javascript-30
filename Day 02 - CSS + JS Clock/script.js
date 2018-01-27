ae1c6d7aa3293c7226c1e220152fe30b0bfca164
CachDevs <CachDevs@gmail.com>
2018-01-27 18:52:43 +0100

    Fixed Jumping Hands Bug
    Fixed the big that made the hand rotate to 90deg bacwards by seting the
    transition duration to 0s when the second is 0

--- Day 02 - CSS + JS Clock/script.js
+++ Day 02 - CSS + JS Clock/script.js
@@ -20,10 +20,26 @@ function setDate() {
   const secondDegrees = ((second / 60) * 360) + 90;
   const secondDegrees90 = false;
 
+  // Bug control at 0 seconds
+  skipAnimation(second);
+
   // Moving
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
   minHand.style.transform = `rotate(${minDegrees}deg)`;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+}
+
+function skipAnimation(second) {
+  const transitionDuration = '0.08s';
+  if (second == 0) {
+    hourHand.style.transitionDuration = '0s';
+    minHand.style.transitionDuration = '0s';
+    secondHand.style.transitionDuration = '0s';
+  } else {
+    hourHand.style.transitionDuration = transitionDuration;
+    minHand.style.transitionDuration = transitionDuration;
+    secondHand.style.transitionDuration = transitionDuration;
+  }
 }
 
 setInterval(setDate, 1000);
