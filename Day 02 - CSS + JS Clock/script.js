0f05557698a04b379d134f675eb129d5d9b694ab
CachDevs <CachDevs@gmail.com>
2018-01-29 19:45:40 +0100

    Removed Unnecessary Code

--- Day 02 - CSS + JS Clock/script.js
+++ Day 02 - CSS + JS Clock/script.js
@@ -8,17 +8,14 @@ function setDate() {
   // Calculate hours
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + 90;
-  const hourDegrees90 = false;
 
   // Calculate minutes
   const min = now.getMinutes();
   const minDegrees = ((min / 60) * 360) + 90;
-  const minDegrees90 = false;
 
   // Calculate seconds
   const second = now.getSeconds();
   const secondDegrees = ((second / 60) * 360) + 90;
-  const secondDegrees90 = false;
 
   // Bug control at 0 seconds
   skipAnimation(second);
