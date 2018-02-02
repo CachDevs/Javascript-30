36de107cb0097d15a0fc731d755ba65a4581edc8
CachDevs <CachDevs@gmail.com>
2018-02-02 18:02:08 +0100

    Day 4 Initial Setup

---
+++ Day 04 - Array Cardio Day/script.js
@@ -0,0 +1,48 @@
+// Some data we can work with
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last 'Plank', year: 1858, passed: 1947 },
+];
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick',
+  'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire',
+  'Bellow, Saul', 'Benchley, Robert', 'Benson, Peter', 'Ben-Gurion, David',
+  'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
+  'Bent, Silas', 'Bensten, Lloyd', 'Berger, Ric', 'Berne, Eric', 'Berio, Luciano',
+  'Berle, Milton', 'Berlin, Irving', 'Bernie, Eric', 'Bernhard, Sandra', 'Berra, Yogi',
+  'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken',
+  'Biden, Joshep', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
+  'Birrel, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'
+];
+
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+// 5. Sort the inventors by years lived
+
+// 6. Create a list of Boulevards in Paris that contain 'de' anywhere in the names
+
+// 7. Sort exercise
+// Sort the people alphabetically by last names
+
+// 8. Reduce exercise
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike',
+  'walk', 'car', 'van', 'car', 'truck'
+];
