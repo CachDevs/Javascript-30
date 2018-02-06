70b2964ea4c2848f1ab91c21fc7b64155c13fe79
CachDevs <CachDevs@gmail.com>
2018-02-06 21:00:18 +0100

    Added Color and Shape Changing Functionality
    Each draw changes the context stroke style hue and the dummy variable
    direction, that makes the context line width fluctuate between 85 and 1

--- Day 08 - Fun HTML5 Canvas/script.js
+++ Day 08 - Fun HTML5 Canvas/script.js
@@ -4,23 +4,42 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.strokeStyle = '#BADA55';
 context.lineJoin = 'round';
 context.lineCap = 'round';
+context.lineWidth = 85;
 
 let isDrawing = false;
 let lastX = 0,
     lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return // Stop the function when not drawing
 
+  context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   context.beginPath();
   context.moveTo(lastX, lastY); // Start form
   context.lineTo(e.offsetX, e.offsetY); // Go to
   context.stroke();
 
-  [lastX, lastY] = [e.offsetX, e.offsetY]; // Update line position coordenates
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // Update line position coordinates
+
+  // hue update
+  hue++;
+  if(hue == 360) hue = 0;
+
+  // lineWidth update
+  console.log(context.lineWidth);
+  if(context.lineWidth >= 85 || context.lineWidth <= 1) {
+    console.log("cambio")
+    direction = !direction;
+  }
+  if(direction) {
+    context.lineWidth++;
+  } else {
+    context.lineWidth--;
+  }
 }
 
 canvas.addEventListener('mousedown', (e) => {
