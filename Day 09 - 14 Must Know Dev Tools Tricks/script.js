e6c410624dcfbffb28aead47698c6fe930df8c76
CachDevs <CachDevs@gmail.com>
2018-02-07 20:18:24 +0100

    14 Tricks
    1. Break on option with right click in the dev tools
    2. console.clear
    3. console.log
    4. Interpolated console.log
    5. Styled console.log
    6. console.warn
    7. console.error
    8. console.info
    9. Testing with console.assert
    10. View DOM element with console.dir
    11. console.group and console.groupCollapsed
    12. console.count
    13. console.time and console.timeEnd
    14. console.table

--- Day 09 - 14 Must Know Dev Tools Tricks/script.js
+++ Day 09 - 14 Must Know Dev Tools Tricks/script.js
@@ -4,26 +4,69 @@ function makeGreen() {
   p.style.color = '#BADA55';
   p.style.fontSize = '50px';
 }
-// Regular
 
-// Interpolated
+const p = document.querySelector('p');
 
-// Styled
-
-// warning!
-
-// Error :|
-
-// Info
-
-// Testing
 
 // clearing
+console.clear();
+
+// Regular
+console.log('Hello');
+
+// Interpolated
+console.log('Hello I am a %s string!', 'interpolated');
+
+// Styled
+console.log('%c I am some great text ', 'font-size:20px; text-shadow: 5px 5px 0 grey');
+
+// warning!
+console.warn('I am a warning');
+
+// Error :|
+console.error('I am an error');
+
+// Info
+console.info('I am an info message');
+
+// Testing
+console.assert(1 === 2, 'That is wrong');
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);
 
 // Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+})
 
 // counting
+console.count('One');
+console.count('One');
+console.count('One');
+console.count('Another');
+console.count('One');
+console.count('Another');
+console.count('One');
+console.count('Another');
+console.count('Another');
+console.count('One');
+console.count('Another');
+
 
 // timing
+console.time('Fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(jsonData => {
+    console.log(jsonData);
+    console.timeEnd('Fetching data');
+  });
+
+// table
+console.table(dogs);
